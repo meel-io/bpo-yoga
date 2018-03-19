bf559d6dd6aaf4fbd095c4ce28d4016b961d36e5
e1000-io <emiliodelatorrea@gmail.com>
2018-03-19 00:30:28 +0000

    Fix bugs in mutation

--- utils/schema.js
+++ utils/schema.js
@@ -1,87 +1,84 @@
-const { db } = require("./db");
+const { db } = require("./db")
 
 const getAgents = () => {
-    return db.get("agents").value();
-};
+  return db.get("agents").value()
+}
 
 const setRoomRate = (roomRateId, weekdayRate = 0, weekendRate = 0) => {
-    const rates = [];
-    const agents = db.get("agents").value();
+  const rates = []
+  const agents = db.get("agents").value()
 
-    const agent = agentRateHasRoomRate(agents, roomRateId);
+  const agent = agentRateHasRoomRate(agents, roomRateId)
 
-    const updatedRates = modifyRateRoomRate(agent.rates, {
-        roomRateId,
-        weekdayRate,
-        weekendRate
-    });
+  const updatedRates = modifyRateRoomRate(agent.rates, {
+    roomRateId,
+    weekdayRate,
+    weekendRate
+  })
 
-    db
-        .get("agents")
-        .find({ id: agent.id })
-        .assign({ rates: updatedRates })
-        .write();
+  db
+    .get("agents")
+    .find({ id: agent.id })
+    .assign({ rates: updatedRates })
+    .write()
 
-    return updatedRates;
-};
+  return updatedRates
+}
 
 const agentRateHasRoomRate = (agents, roomRateId) => {
-    return agents.find(agent => {
-            return !!ratesHaveRoomRate(agent.rates, roomRateId);
-});
-};
+  return agents.find(agent => {
+    return !!ratesHaveRoomRate(agent.rates, roomRateId)
+  })
+}
 
 const ratesHaveRoomRate = (rates, roomRateId) => {
-    return rates.find(rate => {
-            if (rate.seasons) {
-        return rate.seasons.find(season => {
-                return !!rateHasRoomRate(season.rooms);
-    });
+  return rates.find(rate => {
+    if (rate.seasons) {
+      return rate.seasons.find(season => {
+        return !!rateHasRoomRate(season.rooms, roomRateId)
+      })
     }
-    return !!rateHasRoomRate(rate.rooms, roomRateId);
-});
-};
+    return !!rateHasRoomRate(rate.rooms, roomRateId)
+  })
+}
 
 const rateHasRoomRate = (rooms, roomRateId) => {
-    return rooms.find(room => {
-            return room.id == roomRateId;
-});
-};
+  return rooms.find(room => {
+    return room.id == roomRateId
+  })
+}
 
-const modifyRateRoomRate = (
-    rates,
-        { roomRateId, weekdayRate, weekendRate }
-) => {
-    const rate = ratesHaveRoomRate(rates, roomRateId);
+const modifyRateRoomRate = (rates, { roomRateId, weekdayRate, weekendRate }) => {
+  const rate = ratesHaveRoomRate(rates, roomRateId)
 
-    if (rate.seasons) {
-        const season = rate.seasons.find(season => {
-                return !!ratesHaveRoomRate(season.rooms);
-    });
+  if (rate.seasons) {
+    const season = rate.seasons.find(season => {
+      return !!rateHasRoomRate(season.rooms, roomRateId)
+    })
 
-        return modifyRoomRate(season.rooms, {
-            roomRateId,
-            weekdayRate,
-            weekendRate
-        });
-    }
+    return modifyRoomRate(season.rooms, {
+      roomRateId,
+      weekdayRate,
+      weekendRate
+    })
+  }
 
-    return modifyRoomRate(rate.rooms, {
-        roomRateId,
-        weekdayRate,
-        weekendRate
-    });
-};
+  return modifyRoomRate(rate.rooms, {
+    roomRateId,
+    weekdayRate,
+    weekendRate
+  })
+}
 
 modifyRoomRate = (rooms, { roomRateId, weekdayRate, weekendRate }) => {
-    const roomRate = rooms.find(room => {
-            return room.id == roomRateId;
-});
+  const roomRate = rooms.find(room => {
+    return room.id == roomRateId
+  })
 
-    roomRate.weekdayRate = weekdayRate;
-    roomRate.weekendRate = weekendRate;
+  roomRate.weekdayRate = weekdayRate
+  roomRate.weekendRate = weekendRate
 
-    return rooms;
-};
+  return rooms
+}
 
-module.exports = { getAgents, setRoomRate };
+module.exports = { getAgents, setRoomRate }
